e6742fcc844154af61ba517b752ef0e249f7012c
chuinga <mfproencamartins@gmail.com>
2024-08-15 15:30:06 +0100

    feat: add for loop

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 number = 5
 text = 'Hello World'
-print(number)
-print(text)
-print(text[6])
-print(text[-1])
-print(len(text))
-print(type(text))
+#print(number)
+#print(text)
+#print(text[6])
+#print(text[-1])
+#print(len(text))
+#print(type(text))
 shift = 3
-print(shift)
-print(type(shift))
+#print(shift)
+#print(type(shift))
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
-print(alphabet.find('z'))
+#print(alphabet.find('z'))
 index = alphabet.find(text[0])
 index = alphabet.find(text[0].lower())
-print(index)
-print(text.lower())
+#print(index)
+#print(text.lower())
+shifted = alphabet[index+shift]
+#print(shifted)
+#print(shift)
+for char in text:
+    print(char)
